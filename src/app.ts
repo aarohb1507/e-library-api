3dfd1cc7f471cf0d967e83b66b3cddc967980a70
Aaroh Bhardwaj <aarohb.1507@gmail.com>
2025-10-21 19:09:54 +0530

    user register endpoint tested

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import express from 'express'
 
 import globalErrorHandler from './middlewares/globalErrorHandler'
+import userRouter from './user/userRouter'
 
 const app = express()
 
 app.get('/', (req, res, next)=>{
- 
-
   res.json({
     message: "Welcome to elib apis"
   })
 })
+
+//register user
+app.use('/api/users', userRouter )
 
 //global error handler
 app.use(globalErrorHandler)
