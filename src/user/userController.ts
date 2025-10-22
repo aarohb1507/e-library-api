00316dc10a924d382bd00ae39f357c8ec8583f0d
Aaroh Bhardwaj <aarohb.1507@gmail.com>
2025-10-23 00:16:51 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -6,6 +6,8 @@ import { config } from "../config/config";
 import jwt from "jsonwebtoken";
 import type { User } from "./userTypes";
 
+
+
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   //validation
   const { name, email, password } = req.body;
@@ -43,6 +45,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   //process
 
   try {
+    //token generation
     const token = jwt.sign({ sub: newUser._id }, config.jwtSecret as string, {
       expiresIn: "7d",
       algorithm: "HS256",
@@ -55,7 +58,39 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   } catch (err) {
     return next(createHttpError(500, "error while signing jwt token"));
   }
-  //token generation
+  
 };
+//LOGIN ENDPOINTS:
 
-export default createUser;
+const loginUser = async(req:Request, res:Response, next:NextFunction)=>{
+  const {email, password} = req.body
+//all fields 
+  if(!email || !password){
+    return next(createHttpError(400, "All fields are required"))
+  }
+//finding if exist
+let user
+  try{
+      user = await userModel.findOne({ email })
+  if(!user){
+    return next(createHttpError(404, "no such email exists"))
+  }
+  }catch(err){
+    return next(createHttpError(500, "error while finding user email"))
+  }
+  //match passwords
+  const isMatch = await bcrypt.compare(password, user.password)
+  if(!isMatch){
+    return next(createHttpError(404, "passwords don't match"))
+  }
+ //token issue
+  
+  res.json({
+    message:"ok dokay"
+  })
+}
+
+
+export {createUser, loginUser};
+
+
